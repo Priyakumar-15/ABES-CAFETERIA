6fa050b3463e471bc1de006c0cfbfd885f47a93c
Your Name <you@example.com>
2025-06-21 21:38:44 +0530

    Add initial backend files and dependencies

---
+++ database/dbConnection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+export const dbConnection=()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+        dbName:"abescafeteria"
+    }).then(()=>{
+        console.log("Connected to database successfully!");
+    }).catch(err =>{
+        console.log(`some error occured while connecting to database! ${err}`);
+    })
+}
